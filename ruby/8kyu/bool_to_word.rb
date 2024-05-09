3c9f01be0557fdd3e25ceeb354e4781560504414
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-05-10 00:28:14 +0800

    solve: bool_to_word

---
+++ ruby/8kyu/bool_to_word.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Complete the moethod that takes a boolean value and return
+# a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(bool)
+  bool ? 'Yes' : 'No'
+end
